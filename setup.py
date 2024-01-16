4cb5dc9ce37a20261bb4a9a961de259d0bf790af
Dimitri Papaioannou <algorythmist@users.noreply.github.com>
2024-01-16 10:02:00 -0800

    fixed CI

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='python-structures',
+    version='0.1',
+    packages=setuptools.find_packages(),
+    url='https://github.com/algorythmist/python-structures',
+    author='Dimitri Papaioannou',
+    description='Data Structures in Python',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10'
+)
